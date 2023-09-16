44843126c130da533150301e82487484d0cf8c99
ChauhanOO7 <saurabhc7388@gmail.com>
2023-09-16 17:28:01 +0530

    first commit

---
+++ Hoisting_lexical.js
@@ -0,0 +1,54 @@
+//Hoisting-  Hoisting is a concept that enables us to use values of variables and functions even before initializing value without getting errors
+//and this happens during the 1st phase (memory creation phase) of the Execution Context.
+
+//But hoisting is different for let/const variables.
+
+//normal code
+var cx=100;
+function num()
+{
+    return 10;
+}
+
+console.log(cx);
+console.log(num());
+
+
+//with hoisting
+
+console.log(cx1);
+console.log(num1());
+
+var cx1=200;
+
+function num1()
+{
+    return 100;
+}
+
+// undefined- it's like a placeholder(or keyword) which takes place in memory until the actual value is not assigned.
+
+// Not defined- when there is no allocation of memory for something then it said to be not defined.
+
+/*Scope- The scope is the current context of execution in which values and expressions are "visible" or can be referenced.
+
+scope of variable- Accessibility of a particular variable within the program.*/
+
+/* lexical environment--
+A lexical environment is a data structure that holds identifier-variable mapping.
+(here identifier refers to the name of variables/functions, and the variable is the reference to actual
+object [including function object or primitive value].*/
+
+/*
+Lexical in general means in hierarchy or in a sequence.Whenever a new execution context(EC) is created a new lexical environment
+is created and it is referenced in local EC in memory space.
+
+Lexical Environment: Local Memory + Lexical Environment of its Parent.
+
+So in short, a lexical environment is a place where variables and functions live or physically present during the program execution.
+*/
+
+// scope chain- A chain form when we move from one lexical to another lexical environment.
+
+
+
---
+++ let_const_tmp.js
@@ -0,0 +1,60 @@
+// let and const are both hoisted but in different way.
+
+/*
+In var-
+1.we can redeclare and updated.
+2.Both global and block scoped.
+
+In let-
+1.we can not redeclare but update it.
+like-
+    let a=10;
+    let a=10;
+2.only block scoped.
+
+In const-
+1.can not redeclare and block scoped.
+2.we can not update it.
+*/
+
+//*If users use the let variable before the declaration, it does not initialize with undefined just like a var variable, and returns an error.
+//like in below example.
+
+/*
+console.log(x);
+console.log(z);
+var x = 6;
+let z = 6;
+console.log(x);
+console.log(z);
+
+*/
+
+/*
+Explanation:
+
+1.First of all the Global execution context will be created.
+2.And then the memory allocation phase starts, During this, the variable x got a place in memory and javascript puts undefined there.
+3.And then the variable z gets space in a different place of memory(TDZ or another place named Script) and same as variable x then undefined wil
+be assigned as value.
+4.Then the thread execution phase starts, During this console.log(x) statement executes and prints the value of x, which is undefined.
+5.In the next line, there is console.log(z), javascript will throw ReferenceError and the program will stop here.
+*/
+
+/*
+temporal dead zone(tdz)- the starting of the execution of block in which the let or const variable is declared till that variable is being 
+initialized is called Temporal Dead Zone for the variable.
+During this zone javascript will always through a reference error if anyone tries to access those variables. 
+*/
+
+//correct code-
+
+let a;  //declare first
+console.log(a);
+a=10;
+console.log(a);
+
+//for const you have to declare and initialize with a value also.
+
+const b=1000;
+console.log(b);
